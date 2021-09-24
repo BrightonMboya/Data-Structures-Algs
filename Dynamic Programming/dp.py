f6c38e14aaeb2e88cf8323c01465dff33ba03899
BrightonMboya <b.mboya@alustudent.com>
2021-09-24 17:14:32 +0200

    implemented a new feature

--- Dynamic Programming/dp.py
+++ Dynamic Programming/dp.py
@@ -1,1 +1,19 @@
-print('hello World')
+def checkMonthCode(month):
+    if month.lower() == 'january' or month.lower() == 'october':
+        monthCode = 0
+    elif month.lower() == 'february' or month.lower() == 'march' or month.lower() == 'november':
+        monthCode = 3
+    elif month.lower() == 'april' or month.lower() == 'july':
+        monthCode = 6
+    elif month.lower() == 'may':
+        monthCode = 1
+    elif month.lower() == 'june':
+        monthCode = 4
+    elif month.lower() == 'august':
+        monthCode = 2
+    else:
+        monthCode = 5
+    return monthCode
+    
+
+checkMonthCode('february')
