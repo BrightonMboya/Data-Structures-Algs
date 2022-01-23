d013cc15df74ad374ec1293e583e8232073a4f1e
BrightonMboya <b.mboya@alustudent.com>
2022-01-23 18:02:50 +0200

    did some leetcode

--- LeetCode/del.py
+++ LeetCode/del.py
@@ -1,5 +1,21 @@
-nums = [5, 1, 2, 3, 4]
-n = len(nums)
-for inx in reversed(range(n-1)):
-    nums[inx], nums[inx-1] = nums[inx-1], nums[inx]
-print(nums)
+def threeNumberSort(array, order):
+	# Write your code here.
+	k = 3
+	while k <= 3:
+		currentNum = order.pop(0)
+		newPos = 0
+		left = newPos
+		for i in range(1, len(array)):
+			if array[left] == currentNum:
+				left += 1
+				newPos += 1
+			if array[i] == currentNum:
+				array[left], array[i] = array[i], array[left]
+				left += 1
+				newPos += 1
+		k -= 1
+	return array
+
+# 0 0 0 1 1 -1 -1
+
+threeNumberSort([1, 0, 0, -1, -1, 0, 1, 1], [0, 1, -1])
